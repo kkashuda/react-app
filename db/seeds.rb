a35dd4bcf9e21e860b18b5da4deea3da678a4fb1
kkashuda <kkashuda@gmail.com>
2017-07-20 15:18:01 -0500

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+prosecco = Drink.create(
+  title: "Rasberry  Limoncello Prosecco",
+  description: "Amazingly refreshing, bubbly, and sweet – a perfect summer cocktail that you can make in just 5 minutes!",
+  steps: "In a large pitcher, whisk together prosecco and limoncello. Serve over raspberries, garnished with mint, if desired.",
+  source: "http://damndelicious.net/2014/06/09/raspberry-limoncello-prosecco/",
+)
+
+prosecco.ingredients.create(description: "3 cups prosecco, chilled")
+prosecco.ingredients.create(description: "1 cup limoncello liqueur, chilled")
+prosecco.ingredients.create(description: "1 cup frozen raspberries")
+prosecco.ingredients.create(description: "6 sprigs fresh mint")
