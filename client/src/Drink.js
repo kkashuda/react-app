dff710129d59541b9a21f015bbdc7379d3963f6d
kkashuda <kkashuda@gmail.com>
2017-07-20 17:48:11 -0500

    create files for stateless components. Render header on drink page using semantic-ui

---
+++ client/src/Drink.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react'
+
+// :title, :description, :steps, :source
+const Drink = ({title, description, steps, source}) => (
+    <Container text>
+          <Header as='h2'>Header</Header>
+        <p></p>
+    </Container>
+)
+
+export default Drink
