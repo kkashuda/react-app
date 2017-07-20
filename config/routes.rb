8beae942954f46f603a0d950494148d46979562e
kkashuda <kkashuda@gmail.com>
2017-07-20 15:07:22 -0500

    add drink and ingredients model + let active admin know about them

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :ingredients
+  resources :drinks
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   #ActiveAdmin.routes(self)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720195342) do
+ActiveRecord::Schema.define(version: 20170720200546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,4 +46,22 @@ ActiveRecord::Schema.define(version: 20170720195342) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "drinks", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "steps"
+    t.string   "source"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.integer  "drink_id"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["drink_id"], name: "index_ingredients_on_drink_id", using: :btree
+  end
+
+  add_foreign_key "ingredients", "drinks"
 end
