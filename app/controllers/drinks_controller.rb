de0c980ee098b6659653a5d3dd68beeed1cc0af4
kkashuda <kkashuda@gmail.com>
2017-07-20 15:30:01 -0500

    set up Procfile to run the create-app setup locally

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -3,14 +3,14 @@ class DrinksController < ApplicationController
 
   # GET /drinks
   def index
-    @drinks = Drink.all
-
-    render json: @drinks
+    @drinks = Drink.select("id, title").all
+    render json: @drinks.to_json
   end
 
   # GET /drinks/1
   def show
-    render json: @drink
+     @drink = Drink.find(params[:id])
+    render json: @drink.to_json(:include => { :ingredients => { :only => [:id, :description] }})
   end
 
   # POST /drinks
