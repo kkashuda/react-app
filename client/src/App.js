9ce466c7e534acb8f219e65a9791834ea47f4b0f
kkashuda <kkashuda@gmail.com>
2017-07-20 17:21:35 -0500

    install semantic UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react'
 
 class App extends Component {
-  componentDidMount() {
-    window.fetch('api/drinks')
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(error => console.log(error))
-  }
+  
   render() {
     return (
       <div className="App">
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.css'
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
